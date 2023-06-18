c2055452edc83997e29a56a8e6ea6e1b171b3a74
David Liu <davidliu@kinetics.com.tw>
2023-06-18 21:57:06 +0800

    20230618_2157

---
+++ EmployeeServices/EmployeeServices/DTO/EmployeeDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace EmployeeServices.DTO
+{
+    public class EmployeeDTO
+    {
+        public int EmployeeID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Title { get; set; }
+    }
+}
---
+++ EmployeeServices/EmployeeServices/Program.cs
@@ -0,0 +1,31 @@
+using EmployeeServices.Models;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+var NorthwindconnectionString = builder.Configuration.GetConnectionString("Northwind");
+builder.Services.AddDbContext<NorthwindContext>(options =>
+    options.UseSqlServer(NorthwindconnectionString));
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
